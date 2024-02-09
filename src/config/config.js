a302b9bf930f0cae11e90054363f52115b47752d
Nikunjkanteliya <kanteliyanikunj422@gmail.com>
2024-02-09 11:52:46 +0530

    hosted url added

--- src/config/config.js
+++ src/config/config.js
@@ -1,141 +1,140 @@
-
-
- export let hardcodeData = [
-    {
-      foodId: "food_bwrgmmqau78xrdazxx79obeezumz",
-      uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01001",
-      label: "Butter, Salted",
-      knownAs: "Butter, salted",
-      nutrients: {
-        ENERC_KCAL: 717.0,
-        PROCNT: 0.85,
-        FAT: 81.1,
-        CHOCDF: 0.06,
-        FIBTG: 0.0,
-      },
-      category: "Generic foods",
-      categoryLabel: "food",
-      image:
-        "https://www.edamam.com/food-img/515/515af390107678fce1533a31ee4cc35b.jpeg",
+export let hardcodeData = [
+  {
+    foodId: "food_bwrgmmqau78xrdazxx79obeezumz",
+    uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01001",
+    label: "Butter, Salted",
+    knownAs: "Butter, salted",
+    nutrients: {
+      ENERC_KCAL: 717.0,
+      PROCNT: 0.85,
+      FAT: 81.1,
+      CHOCDF: 0.06,
+      FIBTG: 0.0,
     },
-    {
-      foodId: "food_blrpqo1bp94h17apq4prgbhcuhex",
-      uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01002",
-      label: "Butter, Whipped, With Salt",
-      knownAs: "Butter, whipped, with salt",
-      nutrients: {
-        ENERC_KCAL: 731.0,
-        PROCNT: 0.49,
-        FAT: 78.3,
-        CHOCDF: 0.0,
-        FIBTG: 0.0,
-      },
-      category: "Generic foods",
-      categoryLabel: "food",
-      image:
-        "https://www.edamam.com/food-img/ec6/ec68f0d43ed9b399c8464481f89eb225.jpg",
+    category: "Generic foods",
+    categoryLabel: "food",
+    image:
+      "https://www.edamam.com/food-img/515/515af390107678fce1533a31ee4cc35b.jpeg",
+  },
+  {
+    foodId: "food_blrpqo1bp94h17apq4prgbhcuhex",
+    uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01002",
+    label: "Butter, Whipped, With Salt",
+    knownAs: "Butter, whipped, with salt",
+    nutrients: {
+      ENERC_KCAL: 731.0,
+      PROCNT: 0.49,
+      FAT: 78.3,
+      CHOCDF: 0.0,
+      FIBTG: 0.0,
     },
-    {
-      foodId: "food_bzaenytbm02cd4br159f4biv49jc",
-      uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01003",
-      label: "Butter Oil, Anhydrous",
-      knownAs: "Butter oil, anhydrous",
-      nutrients: {
-        ENERC_KCAL: 876.0,
-        PROCNT: 0.28,
-        FAT: 99.5,
-        CHOCDF: 0.0,
-        FIBTG: 0.0,
-      },
-      category: "Generic foods",
-      categoryLabel: "food",
-      image:
-        "https://www.edamam.com/food-img/2b5/2b504c036c64481b224c9d74cc4a82e0.jpg",
+    category: "Generic foods",
+    categoryLabel: "food",
+    image:
+      "https://www.edamam.com/food-img/ec6/ec68f0d43ed9b399c8464481f89eb225.jpg",
+  },
+  {
+    foodId: "food_bzaenytbm02cd4br159f4biv49jc",
+    uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01003",
+    label: "Butter Oil, Anhydrous",
+    knownAs: "Butter oil, anhydrous",
+    nutrients: {
+      ENERC_KCAL: 876.0,
+      PROCNT: 0.28,
+      FAT: 99.5,
+      CHOCDF: 0.0,
+      FIBTG: 0.0,
     },
-    {
-      foodId: "food_aqptx2cauhjjzubmplnjna81ev33",
-      uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01004",
-      label: "Cheese, Blue",
-      knownAs: "Cheese, blue",
-      nutrients: {
-        ENERC_KCAL: 353.0,
-        PROCNT: 21.4,
-        FAT: 28.7,
-        CHOCDF: 2.34,
-        FIBTG: 0.0,
-      },
-      category: "Generic foods",
-      categoryLabel: "food",
-      image:
-        "https://www.edamam.com/food-img/b44/b442cfc174a1a691dca574c9b7dcb47a.jpg",
+    category: "Generic foods",
+    categoryLabel: "food",
+    image:
+      "https://www.edamam.com/food-img/2b5/2b504c036c64481b224c9d74cc4a82e0.jpg",
+  },
+  {
+    foodId: "food_aqptx2cauhjjzubmplnjna81ev33",
+    uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01004",
+    label: "Cheese, Blue",
+    knownAs: "Cheese, blue",
+    nutrients: {
+      ENERC_KCAL: 353.0,
+      PROCNT: 21.4,
+      FAT: 28.7,
+      CHOCDF: 2.34,
+      FIBTG: 0.0,
     },
-    {
-      foodId: "food_au4tus5b092owgbiu8yama5yupfp",
-      uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01005",
-      label: "Cheese, Brick",
-      knownAs: "Cheese, brick",
-      nutrients: {
-        ENERC_KCAL: 371.0,
-        PROCNT: 23.2,
-        FAT: 29.7,
-        CHOCDF: 2.79,
-        FIBTG: 0.0,
-      },
-      category: "Generic foods",
-      categoryLabel: "food",
-      image:
-        "https://www.edamam.com/food-img/702/7023ac63ef897bab1f6ea399316748d7.png",
+    category: "Generic foods",
+    categoryLabel: "food",
+    image:
+      "https://www.edamam.com/food-img/b44/b442cfc174a1a691dca574c9b7dcb47a.jpg",
+  },
+  {
+    foodId: "food_au4tus5b092owgbiu8yama5yupfp",
+    uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01005",
+    label: "Cheese, Brick",
+    knownAs: "Cheese, brick",
+    nutrients: {
+      ENERC_KCAL: 371.0,
+      PROCNT: 23.2,
+      FAT: 29.7,
+      CHOCDF: 2.79,
+      FIBTG: 0.0,
     },
-    {
-      foodId: "food_a06hreabh7agoeazgrtiobunge7b",
-      uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01006",
-      label: "Cheese, Brie",
-      knownAs: "Cheese, brie",
-      nutrients: {
-        ENERC_KCAL: 334.0,
-        PROCNT: 20.8,
-        FAT: 27.7,
-        CHOCDF: 0.45,
-        FIBTG: 0.0,
-      },
-      category: "Generic foods",
-      categoryLabel: "food",
-      image:
-        "https://www.edamam.com/food-img/f53/f5340600239cf508f17b167265c6a1cf.jpg",
+    category: "Generic foods",
+    categoryLabel: "food",
+    image:
+      "https://www.edamam.com/food-img/702/7023ac63ef897bab1f6ea399316748d7.png",
+  },
+  {
+    foodId: "food_a06hreabh7agoeazgrtiobunge7b",
+    uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01006",
+    label: "Cheese, Brie",
+    knownAs: "Cheese, brie",
+    nutrients: {
+      ENERC_KCAL: 334.0,
+      PROCNT: 20.8,
+      FAT: 27.7,
+      CHOCDF: 0.45,
+      FIBTG: 0.0,
     },
-    {
-      foodId: "food_bq1kaa2anb8wmsaj3in43bxxsjyh",
-      uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01007",
-      label: "Cheese, Camembert",
-      knownAs: "Cheese, camembert",
-      nutrients: {
-        ENERC_KCAL: 300.0,
-        PROCNT: 19.8,
-        FAT: 24.3,
-        CHOCDF: 0.46,
-        FIBTG: 0.0,
-      },
-      category: "Generic foods",
-      categoryLabel: "food",
-      image:
-        "https://www.edamam.com/food-img/419/419fc950e8f2a589a11d63efb28b8bef.jpg",
+    category: "Generic foods",
+    categoryLabel: "food",
+    image:
+      "https://www.edamam.com/food-img/f53/f5340600239cf508f17b167265c6a1cf.jpg",
+  },
+  {
+    foodId: "food_bq1kaa2anb8wmsaj3in43bxxsjyh",
+    uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01007",
+    label: "Cheese, Camembert",
+    knownAs: "Cheese, camembert",
+    nutrients: {
+      ENERC_KCAL: 300.0,
+      PROCNT: 19.8,
+      FAT: 24.3,
+      CHOCDF: 0.46,
+      FIBTG: 0.0,
     },
-    {
-      foodId: "food_a8q8p7tahfewtuajrmgcrbpxtti8",
-      uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01008",
-      label: "Cheese, Caraway",
-      knownAs: "Cheese, caraway",
-      nutrients: {
-        ENERC_KCAL: 376.0,
-        PROCNT: 25.2,
-        FAT: 29.2,
-        CHOCDF: 3.06,
-        FIBTG: 0.0,
-      },
-      category: "Generic foods",
-      categoryLabel: "food",
-      image:
-        "https://www.edamam.com/food-img/541/541e46e44ba61aec8bcd599df94c0eed.jpg",
+    category: "Generic foods",
+    categoryLabel: "food",
+    image:
+      "https://www.edamam.com/food-img/419/419fc950e8f2a589a11d63efb28b8bef.jpg",
+  },
+  {
+    foodId: "food_a8q8p7tahfewtuajrmgcrbpxtti8",
+    uri: "http://www.edamam.com/ontologies/edamam.owl#Food_01008",
+    label: "Cheese, Caraway",
+    knownAs: "Cheese, caraway",
+    nutrients: {
+      ENERC_KCAL: 376.0,
+      PROCNT: 25.2,
+      FAT: 29.2,
+      CHOCDF: 3.06,
+      FIBTG: 0.0,
     },
-  ];
-  export let notfoundImg = "https://static.vecteezy.com/system/resources/previews/022/752/919/large_2x/delivery-guy-taking-break-empty-state-illustration-editable-404-not-found-for-ux-ui-design-fast-food-restaurant-isolated-flat-cartoon-character-on-white-error-flash-message-for-website-app-vector.jpg"
+    category: "Generic foods",
+    categoryLabel: "food",
+    image:
+      "https://www.edamam.com/food-img/541/541e46e44ba61aec8bcd599df94c0eed.jpg",
+  },
+];
+export let notfoundImg =
+  "https://i.ibb.co/h24Ys6t/notfound-Photoroom-png-Photoroom.png";
